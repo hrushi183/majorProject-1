868d089d765dfbbf32c3ff3b6eec44b41145d257
hrushi183 <44707285+hrushi183@users.noreply.github.com>
2021-06-18 13:33:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ var usersRouter = require('./routes/users');
 
 // Sessions 
 var sessionStore = new MongoDBStore({
-  uri: 'mongodb+srv://hrushikesh:hrushi123@cluster0.kueh2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority/chefs',
+  uri: 'mongodb+srv://hrushikesh:hrushi123@cluster0.kueh2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   collection: 'sessions'
   });
   // function(error)
